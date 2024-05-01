f99f4db3775e9e653e6a846a8469760ad97926e4
linzhenghao233 <1392872316@qq.com>
2024-05-01 21:11:26 +0800

    添加项目文件。

---
+++ test.c
@@ -0,0 +1,9 @@
+long long quick_pow_mod(long long x, long long n, long long mod) {
+    long long res = 1;
+    while (n > 0) {
+        if (n & 1) res = res * x % mod;
+        x = x * x % mod;
+        n >>= 1;
+    }
+    return res;
+}
